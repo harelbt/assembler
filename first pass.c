04c7431c35baa27835c2f375e7295d0844f6831d
harelbt <harelbtafamh@gmail.com>
2020-07-01 16:56:38 +0300

    a

--- first pass.c
+++ first pass.c
@@ -1,14 +1,15 @@
 #include "first pass.h"
 int first_pass(char* file){
     FILE* filep = open_file(file,"r");
+    /*reading the file and handles everything*/
     while (read_line(filep) != EOF) {}
 }
 int read_line(FILE* file){
     char* line = get_line_dynamic(file);
     if (!strcmp(line, ""))
         return EOF;
-    /*ERROR HANDLING HERE*/
     /*RECOGNIZE SENTENCE TYPE*/
+    /*ERROR HANDLING HERE*/
     /*HANDLE SYMBOL*/
     /*HANDLE (OPERATORS AND OPERANDS) OR DATA*/
 }
