9b7b4de22919734addef7f11d0528b9042b4cb57
harelbt <harelbtafamh@gmail.com>
2020-07-01 18:28:42 +0300

    i index starts at 1 instead of 0 :)

--- assembler.c
+++ assembler.c
@@ -3,7 +3,7 @@
 #include "assembler data.h"
 #include "first pass.h"
 int main (int argc, char* argv[]){
-    int i = 0;
+    int i = 1;
     if (argc == 1) {
         stop(EXIT_FAILURE, "no assembly code was supplied");
     }
