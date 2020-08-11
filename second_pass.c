e68a3143944d8af4b012709bc46dc987678f4dc7
gavri23 <46407245+gavri23@users.noreply.github.com>
2020-08-11 23:49:38 +0300

    finishing the second path including hexa.

--- second_pass.c
+++ second_pass.c
@@ -3,7 +3,6 @@
 #include "helpfunctions.h"
 #include "first pass.h"
 #include "second_pass.h"
-#include "second pass.h"
 #include "errors.h"
 #include "assembler data types.h"
 #include "symbol table.h"
@@ -20,7 +19,9 @@ static void code_symbols(FILE* machine_code, symbol* symbol_table){
     char symbol_to_code[LABEL_MAX_LENGTH];
     char current_address_label[LABEL_MAX_LENGTH];
     int curr_char;
-    unsigned int label_address :24;
+    struct{
+        unsigned int label_translated :24;
+    } label;
     void * symbolPtr;
     int distance;
     symbol* sym;
@@ -32,7 +33,9 @@ static void code_symbols(FILE* machine_code, symbol* symbol_table){
             fscanf(start_line,"%s",current_address_label);
         }
         if(curr_char == '?'){
+            fprintf(machine_code," ");//overriding the question mark
             if(fgetc(machine_code) == '&'){//need to put the distance of label
+                fprintf(machine_code," ");//overriding the & sign
                 fscanf(machine_code,"%s",symbol_to_code);
                 symbolPtr = get_symbol(symbol_table,symbol_to_code,&symbol_table);
                 sym = (struct symbol_table*)symbolPtr;
@@ -40,39 +43,36 @@ static void code_symbols(FILE* machine_code, symbol* symbol_table){
                     distance = atoi(current_address_label) - sym->address;
                     if(distance<0){
                         distance = abs(distance);
-                        label_address = negate_to_binary(distance);
-                        (label_address << 3)|A;//turn on the A bit
-
+                        label.label_translated = negate_to_binary(distance);
                     }
-                    else
-                        label_address = ((distance << 3)|A);
-                    writeHexa(label_address,machine_code);/*write in hexa the address*/
-
-
+                    label.label_translated = ((distance << 3)|A);//turn on the A bit
+                    fprintf(machine_code,"%06x",label.label_translated);/*write in hexa the label address*/
                 }
                 else{
                     fprintf(stderr,"The label %s does not exist in the file",symbol_to_code); /*throw an error that there is no label*/
                 }
-
                 }
             else{//need to put in  address of the label
                 fscanf(machine_code,"%s",symbol_to_code);
                 symbolPtr = get_symbol(symbol_table,symbol_to_code,&symbol_table);
+                sym = (struct symbol_table*)symbolPtr;
                 if(strcmp(sym->name,symbol_to_code)){
-                    label_address = sym->address;
-                    writeHexa(label_address,machine_code);/*write in hexa the label address*/
+                    label.label_translated = sym->address;
+                    fprintf(machine_code,"%06x",label.label_translated);/*write in hexa the label address*/
+
                 }
                 else{
                     fprintf(stderr,"The label %s does not exist in the file",symbol_to_code);/* throw an error that there is no label with such name*/
                 }
             }
-
+            while((fgetc(machine_code)) != '\n'){//deleting all the writing that is not part of the translated label
+                fprintf(machine_code," ");
+            }
         }
+
     }
 }
-void writeHexa(unsigned int label,FILE * file){
 
-}
 static void add_entries(){}
 static void prepare_output_files(){}
 
--- second_pass.h
+++ second_pass.h
@@ -3,13 +3,8 @@
 #define E 1
 #define R 2
 #define A 4
-typedef struct label_position{
-    unsigned int BINARY_LABEL:21;
-    unsigned int ARE:3;
-}label_address;
 int second_pass(FILE * first_pass_file,symbol * symbol_table,FILE * input_file);
 void resetString(char str[STRING_LEN]);
-void writeHexa(unsigned int label_address,FILE * machine_code);
 #ifndef ASSEMBLER_SECOND_PASS_H
 #define ASSEMBLER_SECOND_PASS_H
 #include <stdio.h>
